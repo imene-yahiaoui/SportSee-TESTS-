606379848665a1c649706a91eecb650af8e41b75
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2024-01-16 21:39:19 +0100

    update css Radial BarChart Component

--- FrontEnd/src/components/radialBarChart/index.tsx
+++ FrontEnd/src/components/radialBarChart/index.tsx
@@ -1,42 +1,86 @@
 import React from "react";
-
 import "./style.css";
-const data = 33;
+import {
+  ResponsiveContainer,
+  PolarAngleAxis,
+  RadialBarChart,
+  RadialBar,
+  Legend,
+} from "recharts";
+// import PropTypes from "prop-types";
 
-const CircleChart = ({ percentage }) => {
-  const radius = 80;
-  const circumference = 2 * Math.PI * radius;
-  const offset = circumference - (percentage / 100) * circumference;
+const CustomizedLegend = ({ payload }) => {
+  if (payload && payload.length) {
+    return (
+      <div className="custom-legend">
+        <h1 className="desc">{payload[0].value + "%"}</h1>
+        <p>de votre</p>
+        <p>objectif</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+function RadialBarChartComponent() {
+  const score = 30;
+
+  // let score = [{ "name": "score", "value": (0.3 * 100) }]
 
   return (
-    <svg height="200" width="200" className="CircleChart">
-      <circle
-        cx="100"
-        cy="100"
-        r={radius}
-        stroke="#FF0000"
-        strokeWidth="10"
-        fill="blue"
-        strokeDasharray={circumference}
-        strokeDashoffset={offset}
-        transform={`rotate(190 100 100)`}
-      />
-      <text
-        x="50%"
-        y="50%"
-        textAnchor="middle"
-        dy="0.3em"
-        fontSize="20"
-        fill="#FF0000"
-      >
-        {`${percentage}%`}
-      </text>
-    </svg>
-  );
-};
+    <article className="circleChart">
+      <h1 className="score">Score</h1>
+      <div className="path"></div>
 
-const RadialBarChartComponent = () => {
-  return <CircleChart width="30%" height="100%" percentage={data} />;
-};
+      <ResponsiveContainer>
+        <RadialBarChart
+          width={"100%"}
+          height={"100%"}
+          innerRadius="72%"
+          outerRadius="85%"
+          data={score}
+          startAngle={90}
+          endAngle={450}
+        >
+          <PolarAngleAxis
+            type="number"
+            domain={[0, 100]}
+            dataKey={"value"}
+            angleAxisId={0}
+            tick={false}
+          />
+
+          <RadialBar
+            minAngle={5}
+            fill="#E60000"
+            background={{ fill: "#fff" }}
+            position="center"
+            clockWise={true}
+            dataKey="value"
+            legendType="square"
+            data={score}
+            cornerRadius="50%"
+          />
+
+          <Legend
+            iconSize={10}
+            width={20}
+            height={20}
+            layout="vertical"
+            verticalAlign="top"
+            align="center"
+            payload={score}
+            content={<CustomizedLegend />}
+          />
+        </RadialBarChart>
+      </ResponsiveContainer>
+    </article>
+  );
+}
+
+// RadialBarChartComponent.propTypes = {
+//   data: PropTypes.object.isRequired,
+// };
 
 export default RadialBarChartComponent;
