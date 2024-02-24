f632f2c8e9fea19a2c402acffed3fcf0be93b6f5
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2024-02-24 18:17:40 +0100

    add responsive navbar

--- FrontEnd/src/containers/header/index.tsx
+++ FrontEnd/src/containers/header/index.tsx
@@ -4,29 +4,51 @@
  * @component
  * @returns {JSX.Element} - The rendered Header component.
  */
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import "./style.css";
 
 const Header: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="header">
+    <div className="navbar">
       <Link to="/">
-        <img className="logo" src={logo} alt="logo Kasa" />
+        <img className="logo" src={logo} alt="logo" />
       </Link>
-      <ul className="nav">
+      <div
+        className={`menu-icon ${isMenuOpen ? "open" : ""}`}
+        onClick={toggleMenu}
+      >
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <ul className={`nav ${isMenuOpen ? "open" : ""}`}>
         <li>
-          <Link to="/">Accueil</Link>
+          <Link to="/" onClick={toggleMenu}>
+            Accueil
+          </Link>
         </li>
         <li>
-          <Link to="/profil/12">Profil</Link>
+          <Link to="/profil/12" onClick={toggleMenu}>
+            Profil
+          </Link>
         </li>
         <li>
-          <Link to="/settings">Réglage</Link>
+          <Link to="/settings" onClick={toggleMenu}>
+            Réglage
+          </Link>
         </li>
         <li>
-          <Link to="/community">Communauté</Link>
+          <Link to="/community" onClick={toggleMenu}>
+            Communauté
+          </Link>
         </li>
       </ul>
     </div>
